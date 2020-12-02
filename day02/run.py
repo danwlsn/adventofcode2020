e2dbddb97bf26424f01a1d9e11053c4b31efd533
Danny Wilson <dannywilson32@gmail.com>
2020-12-02 16:07:31 +0000

    day two part two

--- day02/run.py
+++ day02/run.py
@@ -25,13 +25,27 @@ def is_valid(password_parts):
     return is_between(ch_count, ch_range)
 
 
+def is_valid_2(password_parts):
+    ch_index = password_parts[0].split('-')
+    char = password_parts[1].strip(':')
+    password = password_parts[2]
+    valid = False
+
+    if password[int(ch_index[0]) - 1] == char and password[int(ch_index[1]) - 1] != char:
+        valid = True
+    elif password[int(ch_index[1]) - 1] == char and password[int(ch_index[0]) - 1] != char:
+            valid = True
+
+    return valid
+
+
 def main():
     valid_password_count = 0
 
     inputs = read_inputs('input.txt')
     for i in inputs:
         password = get_password(i)
-        if is_valid(password):
+        if is_valid_2(password):
             print(f'valid password: {password[2]}')
             valid_password_count += 1
 
